b590ccfcba553cce2a407b9915861492af2c03fa
Patrick Vianna <patrickviannapblv@gmail.com>
2019-12-23 15:42:46 -0300

    Refactoring UserDto and Automapper

--- src/Api.CrossCutting/Mappings/EntityToDtoProfile.cs
+++ src/Api.CrossCutting/Mappings/EntityToDtoProfile.cs
@@ -1,7 +1,13 @@
-namespace Api.CrossCutting.Mappings
-{
-    public class EntityToDtoProfile
-    {
-        
+using Api.Domain.Dtos.User;
+using Api.Domain.Entities;
+using AutoMapper;
+
+namespace Api.CrossCutting.Mappings {
+    public class EntityToDtoProfile : Profile {
+        public EntityToDtoProfile () {
+            CreateMap<UserDto, UserEntity> ().ReverseMap ();
+
+            CreateMap<UserResultDto, UserEntity> ().ReverseMap ();
+        }
     }
 }
--- src/Api.Domain/Dtos/User/UserDto.cs
+++ src/Api.Domain/Dtos/User/UserDto.cs
@@ -1,7 +1,14 @@
-namespace Api.Domain.Dtos.User
-{
-    public class UserDto
-    {
-        
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.Dtos.User {
+    public class UserDto {
+        [Required (ErrorMessage = "Nome é um campo obrigatório. ")]
+        [StringLength (60, ErrorMessage = "Nome deve ter no máximo {1} caracteres.")]
+        public string Name { get; set; }
+
+        [Required (ErrorMessage = "Nome é um campo obrigatório. ")]
+        [EmailAddress (ErrorMessage = "E-mail em formato inválido.")]
+        [StringLength (100, ErrorMessage = "Nome deve ter no máximo {1} caracteres.")]
+        public string Email { get; set; }
     }
 }
